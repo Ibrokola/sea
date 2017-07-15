5caf8faa59a717bbdf9d8d3e9be1e9a262363608
ibrobabs <babskolawole@gmail.com>
2017-07-15 12:00:42 -0600

    update to homepage

--- home/views.py
+++ home/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 from forum.models import Post
 
 def homepage(request):	
-	if request.user.is_authenticated():
-		user = request.user
-		posts = Post.objects.recent_posts_with_my_votes(user)
-		template = 'forum/home_member.html'
-		context = {"posts":posts}
-		return render(request, template, context)
-	template = 'forum/home.html'
-	context = {}
-	return render(request, template, context)
+    if request.user.is_authenticated():
+        user = request.user
+        posts = Post.objects.recent_posts_with_my_votes(user)
+        template = 'forum/home_member.html'
+        context = {"posts":posts}
+        return render(request, template, context)
+    template = 'forum/home.html'
+    context = {}
+    return render(request, template, context)
