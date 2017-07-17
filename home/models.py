1d7d3cdcbff5ca7ccc506a786d0753af50561b87
ibrobabs <babskolawole@gmail.com>
2017-07-17 14:07:47 -0600

    Homepage carousel and marketing added

--- home/models.py
+++ home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+# Homepage carousel to be curated
+class HomeCarousel(models.Model):
+    title       = models.CharField(max_length=120)
+    image       = models.ImageField()
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+          
+# Homepage marketing to be curated
+class Marketing(models.Model):
+    title   = models.CharField(max_length=200)
+    image   = models.ImageField()
+    reason  = models.CharField(max_length=300) 
+
+    def __str__(self):
+        return self.title 
