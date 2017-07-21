cd5c01e1fcd2faebb740ff3115cfa07b1e22145a
ibrobabs <babskolawole@gmail.com>
2017-07-21 13:43:10 -0600

    update

--- users/views.py
+++ users/views.py
@@ -27,7 +27,8 @@ User = get_user_model()
 class ProfileUserView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         # user = get_object_or_404(User, username=request.user)
-        post = Post.objects.filter(author__username__iexact=request.user).order_by('-submission_time')
+        user = request.user.id
+        post = Post.objects.filter(author__username__iexact=user).order_by('-submission_time')
         # paginator = Paginator(post_list, 10)
         # page = request.GET.get('page')
         
@@ -37,7 +38,7 @@ class ProfileUserView(LoginRequiredMixin, View):
         #      post = paginator.page(1)
         # except EmptyPage:
         #      post = paginator.page(paginator.num_pages)
-        profile = Profile.objects.get(user=request.user)
+        profile = Profile.objects.get(user=user)
         print(profile)
         template = 'users/profile_user.html'
         context = {
