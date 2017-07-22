3cf23d5d20bd106bd69ae8bf951ac7c1ac1d1a56
ibrobabs <babskolawole@gmail.com>
2017-07-22 04:32:55 -0600

    update no more heroku errors

--- users/admin.py
+++ users/admin.py
@@ -3,44 +3,44 @@ from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
-# from .forms import UserChangeForm, UserCreationForm
+from .forms import UserChangeForm, UserCreationForm
 from .models import User 
 
 
-# class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    # form = UserChangeForm
-    # add_form = UserCreationForm
+class UserAdmin(BaseUserAdmin):
+    """The forms to add and change user instances
+    form = UserChangeForm
+    add_form = UserCreationForm
 
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    # list_display = ('username', 'email', 'first_name', 'last_name', 'score', 'is_admin', 'is_active')
-    # list_filter = ('is_admin', 'is_active')
-    # fieldsets = (
-    #     (None, {'fields': ('email', 'password')}),
-    #     ('Personal info', {'fields': ('first_name', 'last_name')}),
-    #     ('Permissions', {'fields': ('is_admin',)}),
-    # )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    # add_fieldsets = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')}
-    #     ),
-    # )
-    # search_fields = ('email', 'username', 'first_name', 'last_name')
-    # ordering = ('username',)
-    # filter_horizontal = ()
+    The fields to be used in displaying the User model.
+    These override the definitions on the base UserAdmin
+    that reference specific fields on auth.User."""
+    list_display = ('username', 'email', 'first_name', 'last_name', 'score', 'is_admin', 'is_active')
+    list_filter = ('is_admin', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+    """add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    overrides get_fieldsets to use this attribute when creating a user."""
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')}
+        ),
+    )
+    search_fields = ('email', 'username', 'first_name', 'last_name')
+    ordering = ('username',)
+    filter_horizontal = ()
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'first_name', 'last_name', 'score', 'is_staff', 'is_active')
-    fields = ('username', 'score', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
-    # readonly_fields = ('username', )
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'email', 'first_name', 'last_name', 'score', 'is_staff', 'is_active')
+#     fields = ('username', 'score', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+#     # readonly_fields = ('username', )
 
 
 admin.site.register(User, UserAdmin)
 # admin.site.register(UserProfile)
 
-# admin.site.unregister(Group)
+admin.site.unregister(Group)
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.signals import user_logged_in
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 
-from django.contrib.auth.models import AbstractUser
+# from django.contrib.auth.models import AbstractUser
 
 
 from django.db import models
@@ -15,113 +15,113 @@ from django.utils import timezone
 
 
 
-# class MyUserManager(BaseUserManager):
-#     def create_user(self, username=None, email=None, password=None):
-#         """
-# 	    Creates and saves a User with the given email, username.
-# 	    """
-#         if not username:
-#             raise ValueError('Must include username')
+class MyUserManager(BaseUserManager):
+    def create_user(self, username=None, email=None, password=None):
+        """
+	    Creates and saves a User with the given email, username.
+	    """
+        if not username:
+            raise ValueError('Must include username')
 
-#         if not email:
-#             raise ValueError('Users must have an email address')
+        if not email:
+            raise ValueError('Users must have an email address')
 
-#         user = self.model(
-#             username = username,
-#             email=self.normalize_email(email),
-#         )
+        user = self.model(
+            username = username,
+            email=self.normalize_email(email),
+        )
 
-#         user.set_password(password)
-#         user.save(using=self._db)
-#         return user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-#     def create_superuser(self, username, email, password):
-#         """
-# 	    Creates and saves a superuser with the given email, username
-# 	    """
-#         user = self.create_user(
-#         	username= username,
-#         	email = email,
-#             password=password
-#         )
-#         user.is_admin = True
-#         user.save(using=self._db)
-#         return user
+    def create_superuser(self, username, email, password):
+        """
+	    Creates and saves a superuser with the given email, username
+	    """
+        user = self.create_user(
+        	username= username,
+        	email = email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
-# class MyUser(AbstractBaseUser):
-#     username = models.CharField(
-#         max_length=255,
-#         unique=True,
-#     )
-#     email = models.EmailField(
-#         verbose_name='email address',
-#         max_length=255,
-#         unique=True,
-#     )
-#     first_name = models.CharField(
-#                 max_length=120,
-#                 null=True,
-#                 blank=True
-#                 )
-#     last_name = models.CharField(
-#                 max_length=120,
-#                 null=True,
-#                 blank=True
-#                 )
-#     score = models.IntegerField(default=0)
-#     # is_member = models.BooleanField(default=False, verbose_name= 'Is Paid_Member')
-#     is_active = models.BooleanField(default=True)
-#     is_admin = models.BooleanField(default=False)
+class User(AbstractBaseUser):
+    username = models.CharField(
+        max_length=255,
+        unique=True,
+    )
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    first_name = models.CharField(
+                max_length=120,
+                null=True,
+                blank=True
+                )
+    last_name = models.CharField(
+                max_length=120,
+                null=True,
+                blank=True
+                )
+    score = models.IntegerField(default=0)
+    # is_member = models.BooleanField(default=False, verbose_name= 'Is Paid_Member')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
 
-#     objects = MyUserManager()
+    objects = MyUserManager()
 
-#     USERNAME_FIELD = 'username'
-#     REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
-#     def get_full_name(self):
-#         # The user is identified by their email address
-#         return "%s %s" %(self.first_name, self.last_name)
+    def get_full_name(self):
+        # The user is identified by their email address
+        return "%s %s" %(self.first_name, self.last_name)
 
-#     def get_short_name(self):
-#         # The user is identified by their email address
-#         return self.first_name
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.first_name
 
-#     def has_perm(self, perm, obj=None):
-#         "Does the user have a specific permission?"
-#         # Simplest possible answer: Yes, always
-#         return True
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
 
-#     def has_module_perms(self, app_label):
-#         "Does the user have permissions to view the app `app_label`?"
-#         # Simplest possible answer: Yes, always
-#         return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
-#     @property
-#     def is_staff(self):
-#         "Is the user a member of staff?"
-#         # Simplest possible answer: All admins are staff
-#         return self.is_admin
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
 
-#     def __str__(self):              # __unicode__ on Python 2
-#         return self.username
+    def __str__(self):              # __unicode__ on Python 2
+        return self.username
      
-#     class Meta:
-#         db_table = "users"
-
-
-
-class User(AbstractUser):
-    #custom user model
     class Meta:
         db_table = "users"
 
-    score = models.IntegerField(default=0)
+
+
+# class User(AbstractUser):
+#     #custom user model
+#     class Meta:
+#         db_table = "users"
+
+#     score = models.IntegerField(default=0)
 
 
 
-    # def __str__(self):              # __unicode__ on Python 2
-    #     return self.username
+#     # def __str__(self):              # __unicode__ on Python 2
+#     #     return self.username
      
 
 
