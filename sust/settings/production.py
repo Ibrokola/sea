b4388a38268f9b90a96661f9935b14b85167ff77
ibrobabs <babskolawole@gmail.com>
2017-08-10 00:23:29 -0600

    update

--- sust/settings/production.py
+++ sust/settings/production.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sustenergy.herokuapp.com']
 
