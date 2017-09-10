12c10ff12db6e9ac81b0ffecc0b8776b18e2a17a
ibrobabs <babskolawole@gmail.com>
2017-09-09 19:41:48 -0600

    update

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import JsonResponse
 from forum.models import Post
-from profiles.models import Profile #UserProfile as
+from profiles.models import Profile
 from .models import HomeCarousel, Marketing
 
 def homepage(request):
@@ -34,19 +34,3 @@ def homepage(request):
     }
     return render(request, template, context)
 
-
-# def listing(request):
-#     contact_list = Contacts.objects.all()
-#     paginator = Paginator(contact_list, 25) # Show 25 contacts per page
-
-#     page = request.GET.get('page')
-#     try:
-#         contacts = paginator.page(page)
-#     except PageNotAnInteger:
-#         # If page is not an integer, deliver first page.
-#         contacts = paginator.page(1)
-#     except EmptyPage:
-#         # If page is out of range (e.g. 9999), deliver last page of results.
-#         contacts = paginator.page(paginator.num_pages)
-
-#     return render(request, 'list.html', {'contacts': contacts})
